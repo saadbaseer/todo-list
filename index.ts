049d4866ac48b40e3054fccc93624de24dc056f0
saadbaseer <saadbaseerkhan410@gmail.com>
2024-04-16 21:49:27 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let todos=[];
+let condition=true;
+while(condition){
+let todoAns=await inquirer.prompt(
+    [
+        {
+            name:"todo",
+            type:"input",
+            message:"what you want to add in your todo list ?"
+        },
+        {
+            name:"addMore",
+            type:"confirm",
+            message:"do you want to add more ?",
+            default:"false"
+        },
+    ]
+)
+
+todos.push(todoAns.todo)
+condition=todoAns.addMore
+console.log(todos)
+
+}
